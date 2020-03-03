f3e8e4cc9cb5277913fa30feb7fea8a642cc6c20
ArfiaLatifah <arfialatifah4@gmail.com>
2020-03-03 18:14:20 +0700

    Membuat halaman utama, match, scorer, dan result. halaman result mengambil data match dan scorer

--- app/src/main/java/id/putraprima/skorbola/ResultActivity.java
+++ app/src/main/java/id/putraprima/skorbola/ResultActivity.java
@@ -3,12 +3,28 @@ package id.putraprima.skorbola;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
+
+    private TextView messageText, scorerText;
+    private String pesan, pemain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        messageText =findViewById(R.id.textView3);
+        scorerText = findViewById(R.id.scorerText);
+
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null){
+            pesan = bundle.getString("MESSAGE_KEY");
+            pemain = bundle.getString("SCORER_KEY");
+            messageText.setText(pesan);
+            scorerText.setText(pemain);
+        }
     }
+
 }
--- app/src/main/java/id/putraprima/skorbola/ScorerActivity.java
+++ app/src/main/java/id/putraprima/skorbola/ScorerActivity.java
@@ -1,14 +1,53 @@
 package id.putraprima.skorbola;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class ScorerActivity extends AppCompatActivity {
+
+    private EditText edit;
+    private int request;
+    private String scorerName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scorer);
+
+        edit = findViewById(R.id.editText);
+
+        Bundle bundle = getIntent().getExtras();
+        if (bundle !=null){
+            request = bundle.getInt("REQUEST_KEY");
+        }
+    }
+
+    public void buttonBack(View view) {
+        scorerName = edit.getText().toString();
+        if ((scorerName).equals("")){
+            Toast.makeText(this, "Anda belum memasukkan nama pemain", Toast.LENGTH_SHORT).show();
+        }else{
+            if (request == 1){
+                scorerName = edit.getText().toString();
+                Intent intent = new Intent();
+                intent.putExtra("HOME_SCORER_KEY", scorerName);
+                setResult(1, intent);
+                finish();
+            }else if (request == 2){
+                scorerName = edit.getText().toString();
+                Intent intent = new Intent();
+                intent.putExtra("AWAY_SCORER_KEY", scorerName);
+                setResult(1, intent);
+                finish();
+            }
+        }
     }
 }
